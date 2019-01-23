18b06b6722e872faece14846cb75609299d9c12e
唐兵 <tangbing@kingchannels.com>
2019-01-23 19:16:46 +0800

    1.23

---
+++ utilstest/src/main/java/com/tangbing/admin/utilstest/JsonParseUtil/ParseUtil.java
@@ -0,0 +1,9 @@
+package com.tangbing.admin.utilstest.JsonParseUtil;
+
+/**
+ * Created by admin on 2019/1/23.
+ */
+
+public class ParseUtil {
+
+}
---
+++ utilstest/src/main/java/com/tangbing/admin/utilstest/JsonParseUtil/UserModel.java
@@ -0,0 +1,12 @@
+package com.tangbing.admin.utilstest.JsonParseUtil;
+
+/**
+ * Created by admin on 2019/1/23.
+ */
+
+public class UserModel {
+    String str1;
+    String str2;
+    String str3;
+    String str4;
+}
