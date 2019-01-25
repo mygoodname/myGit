7c4ba471854ebd8a8602c4b31034bfc43494a7b8
唐兵 <tangbing@kingchannels.com>
2019-01-25 14:52:56 +0800

    加载动画样式

--- utilstest/src/main/java/com/tangbing/admin/utilstest/JsonParseUtil/ParseUtil.java
+++ utilstest/src/main/java/com/tangbing/admin/utilstest/JsonParseUtil/ParseUtil.java
@@ -1,9 +1,22 @@
 package com.tangbing.admin.utilstest.JsonParseUtil;
 
+import com.alibaba.fastjson.JSON;
+
+
+import java.lang.reflect.Type;
+
 /**
  * Created by admin on 2019/1/23.
  */
 
 public class ParseUtil {
-
+    private <T> T parse(String text, Type type) {
+        T t = null;
+        try {
+                t= JSON.parseObject(text, type);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return t;
+    }
 }
--- utilstest/src/main/java/com/tangbing/admin/utilstest/MainActivity.java
+++ utilstest/src/main/java/com/tangbing/admin/utilstest/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+
     }
     private void initView(){
         inText=findViewById(R.id.in_Text);
