767e81968c088399ac913dd69de05583bdbfe1d0
唐兵 <1121354238@qq.com>
2019-02-17 18:14:53 +0800

    测试提交

--- customView/src/main/java/com/tangbing/admin/myfirsttestproject/MainActivity.java
+++ customView/src/main/java/com/tangbing/admin/myfirsttestproject/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+
+
     }
     @Override
     public void onClick(View v) {
