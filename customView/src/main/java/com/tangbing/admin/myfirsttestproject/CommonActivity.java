afaccc1b07562dfee092a3d8d9bc6426b8b59988
唐兵 <tangbing@kingchannels.com>
2019-04-30 10:29:48 +0800

    添加自定义progressBar 界面

--- customView/src/main/java/com/tangbing/admin/myfirsttestproject/CommonActivity.java
+++ customView/src/main/java/com/tangbing/admin/myfirsttestproject/CommonActivity.java
@@ -3,6 +3,10 @@ package com.tangbing.admin.myfirsttestproject;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+
+import com.tangbing.admin.myfirsttestproject.util.ActivityUtils;
 
 /**
  * Created by admin on 2019/3/4.
@@ -13,5 +17,14 @@ public class CommonActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout_commonactivity);
+    }
+    public void goCustomView(View view){
+        ActivityUtils.startActivity(this,CustomViewActivity.class);
+    }
+    public void customDeleteListView(View view){
+        ActivityUtils.startActivity(this,CustomListViwActivity.class);
+    }
+    public void customProgress(View view){
+        ActivityUtils.startActivity(this,CustomProgressActivity.class);
     }
 }
---
+++ customView/src/main/java/com/tangbing/admin/myfirsttestproject/CustomViewActivity.java
@@ -0,0 +1,19 @@
+package com.tangbing.admin.myfirsttestproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tangbing on 2019/4/30.
+ * Describe :
+ */
+
+public class CustomViewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_view);
+    }
+}
