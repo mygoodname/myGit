1bd7f01f0aa11c08164ab6fe4d8d1d4ebc613f5e
唐兵 <1121354238@qq.com>
2020-09-24 16:43:54 +0800

    替换androidx

--- kotlindebug/src/main/java/com/tangbing/admin/kotlindebug/activity/MainActivity.kt
+++ kotlindebug/src/main/java/com/tangbing/admin/kotlindebug/activity/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.tangbing.admin.kotlindebug.activity
 
 import android.content.Intent
-import android.databinding.DataBindingUtil
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
 import android.util.Log;
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.tangbing.admin.kotlindebug.R
 import com.tangbing.admin.kotlindebug.databinding.ActivityMainBinding
 
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mainBinding =DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+        mainBinding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
         mainBinding?.goDetail?.setOnClickListener(View.OnClickListener {
             var intent=Intent(this,ContentActivity::class.java)
             startActivity(intent)
