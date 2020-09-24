8a89c266b0b252caf6ad63376120bd4a6ae7fcc3
唐兵 <tangbing@kingchannels.com>
2020-09-24 18:05:19 +0800

    kotlin 测试

--- kotlindebug/src/main/java/com/tangbing/admin/kotlindebug/activity/MainActivity.kt
+++ kotlindebug/src/main/java/com/tangbing/admin/kotlindebug/activity/MainActivity.kt
@@ -21,12 +21,25 @@ class MainActivity : AppCompatActivity() {
             var intent=Intent(this,ContentActivity::class.java)
             startActivity(intent)
         })
-        Log.d("MainActivity",ContentModel().a)
+        ContentModel().te(1,2,3,4,5)
+        Log.d("MainActivity", ContentModel().sum(1,2).toString())
+//        Log.d("MainActivity", ContentModel().te(1,2,3,4,5).toString())
+        a()
     }
     /**
      *包装log.d日志
      */
     fun d() {
         Log.e("MainActivity", ("1".toInt()==1).toString());
+    }
+    fun a(){
+        var array:ArrayList<String> =ArrayList()
+        array.add("a")
+        array.add("b")
+        array.add("c")
+        array.add("d")
+        array.add("e")
+        ContentModel().tfor(array)
+        ContentModel().twhile()
     }
 }
--- kotlindebug/src/main/java/com/tangbing/admin/kotlindebug/model/ContentModel.kt
+++ kotlindebug/src/main/java/com/tangbing/admin/kotlindebug/model/ContentModel.kt
@@ -1,11 +1,43 @@
 package com.tangbing.admin.kotlindebug.model
 
+import android.util.Log
+import java.util.ArrayList
+
 /**
  * Created by tangbing on 2020/5/7.
  * Describe :
  *
  */
 class ContentModel {
-    lateinit var a:String
+    var a="一条龙服务"
     var b:Int=0
+    fun ta(a:String,b:String)=a+b
+    fun sum(a: Int, b: Int) = a + b
+    fun tb(): Long {
+        return 100L
+    }
+    fun tc() = 100L
+
+    fun te(vararg x:Int){
+        for(vt in x){
+            Log.d("MainActivity", vt.toString())
+        }
+    }
+    fun tfor(a:ArrayList<String>){
+       for (index in a.indices){
+           Log.d("MainActivity", a[index])
+       }
+    }
+    fun twhile(){
+        var index=0
+        while (true){
+            if (index<100){
+                if (index==50)
+                    continue
+                Log.d("MainActivity", index.toString())
+            }else
+                break
+            index++
+        }
+    }
 }
